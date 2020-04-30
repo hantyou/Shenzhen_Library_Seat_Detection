e854d04ca6a5bfd32dd1f9d1aba5776dc851b4bc
hantyou <daniel.zhai@yahoo.com>
2020-05-01 00:03:11 +0800

    实现到了发送验证码的步骤

--- LibDetectRelease.py
+++ LibDetectRelease.py
@@ -1,36 +1,31 @@
 # coding=utf-8
 from selenium import webdriver
 import time
+import winsound
+import tkinter as tk
 from tkinter import messagebox
 import datetime
 import smtplib
 from email.mime.text import MIMEText
 # email 用于构建邮件内容
 from email.header import Header
+
 from win10toast import ToastNotifier
 from multiprocessing import Process
+
 toaster = ToastNotifier()
-"""需要手动添加的库有 selenium, win10toast"""
-# 无icon，采用python的icon，且采用自己的线程
-toaster.show_toast("Python Script Running",
-                   "Script screening library seat is running",
-                   icon_path=None,
-                   duration=20,
-                   threaded=True)
-while toaster.notification_active(): time.sleep(0.1)
-
-
 # 用于构建邮件头
-
+VarValue = ''
+print("程序准备中")
 
 def sendAnEmail(dateText, libname='ShenZhen'):
     print('正在发送邮件')
-
-    from_addr = ''  # 发件人地址
-    password = ''  # 发件人邮箱密码
-    to_addr = ''  # 收件人地址
-
+    print('邮件内容如下')
+    from_addr = ''
+    password = ''
+    to_addr = ''
     smtp_server = ''
+
     text = dateText + 'A seat is available in ' + libname + 'library.'
     print(text)
     msg = MIMEText(text, 'plain', 'utf-8')
@@ -39,7 +34,7 @@ def sendAnEmail(dateText, libname='ShenZhen'):
     msg['To'] = Header(to_addr)
     msg['Subject'] = Header('Available Seat in ' + libname + ' Library')
 
-    server = smtplib.SMTP_SSL()
+    server = smtplib.SMTP_SSL(host=smtp_server)
     server.connect(smtp_server, 465)
     # 登录发信邮箱
     server.login(from_addr, password)
@@ -50,25 +45,58 @@ def sendAnEmail(dateText, libname='ShenZhen'):
     print('邮件成功发送')
 
 
+class MyCollectApp(tk.Toplevel):  # 重点
+    def __init__(self):
+        super().__init__()  # 重点
+        self.setupUI()
+        self.title('验证码')
+
+    def setupUI(self):
+        row1 = tk.Frame(self)
+        row1.pack(fill="x")
+        l1 = tk.Label(row1, text="验证码：", height=2, width=10)
+        l1.pack(side=tk.LEFT)  # 这里的side可以赋值为LEFT  RTGHT TOP  BOTTOM
+        self.xls_text = tk.StringVar()
+        tk.Entry(row1, textvariable=self.xls_text).pack(side=tk.RIGHT)
+
+        row2 = tk.Frame(self)
+        row2.pack(fill="x")
+        tk.Button(row2, text="点击确认", command=self.on_click).pack(side=tk.RIGHT)
+
+    def on_click(self):
+        # print(self.xls_text.get().lstrip())
+        global VarValue
+        VarValue = self.xls_text.get().lstrip()
+        if len(VarValue) == 0:
+            # print("用户名必须输入!")
+            messagebox.showwarning(title='系统提示', message='请输入验证码!')
+            return False
+
+        self.quit()
+        self.destroy()
+
+
 def LibSeatDetect(UN, PW, Libname="ShenZhen",
                   webSite="https://www.szlib.org.cn/m/login.html?formalurl=https%3A%2F%2Fwww.szlib.org.cn%2Fm%2Fmylibrary%2Factivity_appt.jsp%3FcategoryId%3D81"):
     # 配置文件地址
-    print(Libname + "图书馆的位置检测已经开始")
-
+    print(Libname + "图书馆的位置检测已经开始，正在连接网页")
+    global VarValue
     toaster.show_toast("LibDectec Running of " + Libname + "Library",
                        "Script screening library seat of " + Libname + " library is running",
                        icon_path=None,
                        duration=20,
                        threaded=True)
     while toaster.notification_active(): time.sleep(0.1)
-
-    """把下面这一行改成自己的浏览器配置地址"""
     profile_directory = r'C:\Users\Daniel Zhai\AppData\Roaming\Mozilla\Firefox\Profiles\sgjgzcra.default'
     # 加载配置配置
     profile = webdriver.FirefoxProfile(profile_directory)
     # 启动浏览器配置
     drive = webdriver.Firefox(profile)
+
+    # drive = webdriver.Firefox()
     drive.get(webSite)
+    # drive.get("http://www.szlib.org.cn")
+    # drive.get("http://www.baidu.com")
     aa = drive.get_cookies()
     print(aa)
     print(11)
@@ -84,55 +112,106 @@ def LibSeatDetect(UN, PW, Libname="ShenZhen",
     drive.find_element_by_class_name("submit").click()
     SentEmail = False
     failCount = 0
+
     while (1):
+
         failCount += 1
-        print('现在' + Libname + '图书馆有预约名额吗？')
-        print(u"去预约" in drive.page_source)
-        if u"去预约" in drive.page_source:
+        # print('现在' + Libname + '图书馆有预约名额吗？')
+        date = datetime.datetime.now()
+        dateText = str(date.year) + r'-' + str(date.month) + r'-' + str(date.day) + r'-' + str(
+            date.hour) + ':' + str(
+            date.minute) + ':' + str(date.second)
+        # print(dateText)
+        if not u"去预约" in drive.page_source:
+            pass
+            # print('\033[31m没有')
+            # print('\033[30m')
+        else:
+            #def BeepTimes(times):
+            #    for _ in range(times):
+            #        for _ in range(4):
+            #            winsound.Beep(4000,50)
+            #            time.sleep(0.02)
+
+            # b = Process(target=BeepTimes,args=(5,))
+            # b.start()
+            print('\033[32m有空位')
+            print('\033[30m')
             failCount = 0
-            messagebox.showinfo("提示", Libname + "图书馆可以预约啦")
             date = datetime.datetime.now()
             dateText = str(date.year) + r'-' + str(date.month) + r'-' + str(date.day) + r'-' + str(
                 date.hour) + ':' + str(
                 date.minute) + ':' + str(date.second)
             print(dateText)
+            # drive.find_element_by_class_name(".comment_score.gotoyy").click()
+            yuyue = drive.find_element_by_css_selector("[class='comment_score gotoyy']")
+            drive.execute_script("arguments[0].scrollIntoView();", yuyue)
+            yuyue.click()
+            time.sleep(1)
+            drive.find_element_by_id("getCheckCode").click()
             if not SentEmail:
-                toaster.show_toast("A Library Seat in " + Libname + " Library!",
-                                   "A new library seat is available",
-                                   icon_path=None,
-                                   duration=10,
-                                   threaded=True)
-                while toaster.notification_active(): time.sleep(0.1)
                 SentEmail = True
-                """如果需要发送邮件，自行配置下列函数中的参数"""
-                # sendAnEmail(dateText, Libname)
+                try:
+                    sendAnEmail(dateText, Libname)
+                except:
+                    print("发送邮件失败，跳过")
+                    pass
+            app = MyCollectApp()
+            app.mainloop()
+            print(VarValue)
+            drive.find_element_by_css_selector("[class='input w_small required']").click()
+            drive.find_element_by_css_selector("[name='applyCheckCode']").send_keys(VarValue)
+            drive.find_element_by_css_selector("[class='submit btn_2']").click()
+            # drive.find_element_by_name("applyCheckCode").send_keys(VarValue)
+            # drive.find_element_by_class_name("submit btn_2").click()
+
+            break
         if failCount < 10:
-            time.sleep(5)
-            print('No Seat in ' + Libname + ' library, temporarily stop detection for 5 seconds.')
+            for i in range(1, 6):
+                time.sleep(1)
+                last_time = 6 - i
+                print('\rNo Seat in ' + Libname + ' library, temporarily stop detection for %s seconds.'% last_time, end="")
+
         else:
-            print(
-                'No Seat in ' + Libname + ' library for a long time (5 seconds, 10 times), temporarily stop detection for 2 minute.')
-            time.sleep(120)
+            for i in range(1, 121):
+                time.sleep(1)
+                last_time = 121 - i
+                print('\rNo Seat in ' + Libname + ' library for a long time, temporarily stop detection for %s seconds.' % last_time,
+                      end="")
             failCount = 0
         drive.refresh()
     time.sleep(1)
     drive.quit()
+    quit()
 
 
-username = input("请输入读者证号码：")
-password = input("请输入密码：")
-
-listOfLib = [["ShenZhen",
-              "https://www.szlib.org.cn/m/login.html?formalurl=https%3A%2F%2Fwww.szlib.org.cn%2Fm%2Fmylibrary%2Factivity_appt.jsp%3FcategoryId%3D81"],
-             ["FuTian",
-              "https://www.szlib.org.cn/m/mylibrary/activity_appt.jsp?categoryId=84&wxKey=cs_254293649&code=061SWwmi1f6xJr0x8emi1oQbmi1SWwmy&state=1588213084"]]
-l1 = Process(target=LibSeatDetect, args=(username, password, 'ShenZhen', listOfLib[0][1]))
-l2 = Process(target=LibSeatDetect, args=(username, password, 'FuTian', listOfLib[1][1]))
-Processes = []
-
-Processes.append(l1)
-Processes.append(l2)
 if __name__ == '__main__':
+    VarValue = ''
+    # 无icon，采用python的icon，且采用自己的线程
+    toaster.show_toast("Python Script Running",
+                       "Script screening library seat is running",
+                       icon_path=None,
+                       duration=20,
+                       threaded=True)
+    while toaster.notification_active(): time.sleep(0.1)
+    username = input("请输入读者证号码：")
+    password = input("请输入密码：")
+    print(username)
+    print(password)
+    listOfLib = [["ShenZhen",
+                  "https://www.szlib.org.cn/m/login.html?formalurl=https%3A%2F%2Fwww.szlib.org.cn%2Fm%2Fmylibrary%2Factivity_appt.jsp%3FcategoryId%3D81"],
+                 ["FuTian",
+                  "https://www.szlib.org.cn/m/mylibrary/activity_appt.jsp?categoryId=84&wxKey=cs_254293649&code=061SWwmi1f6xJr0x8emi1oQbmi1SWwmy&state=1588213084"]]
+    l1 = Process(target=LibSeatDetect, args=(username, password, 'ShenZhen', listOfLib[0][1]))
+    l2 = Process(target=LibSeatDetect, args=(username, password, 'FuTian', listOfLib[1][1]))
+    flag = input('Choose which library:\n1.Shenzhen\n2.FuTian\nPlease Choose:')
+    Processes = []
+    flag = int(flag)
+    # Processes.append(l1)
+    if flag == 1:
+        Processes.append(l1)
+    if flag == 2:
+        Processes.append(l2)
     for l in Processes:
         l.start()
     for l in Processes:
